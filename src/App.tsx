3e84e77707e8afada9eeffde4882f2b81d6d19ea
Rafiul Islam Hredoy <rafiul.hredoy@technonext.com>
2025-07-02 15:03:01 +0600

    need to figure out how this tailwind differes from the project I created ealier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-green-500 text-white p-4 rounded-xl text-2xl">
+      Tailwind is working 🎉
+    </div>
+  )
+}
+
+export default App
